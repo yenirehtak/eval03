0465e59f00403d5fd7bb576e3d6562fd993cbb7c
Katherine Castro <katherine132000@hotmail.com>
2021-01-29 13:39:11 -0400

    Agregar archivos de proyecto.

---
+++ Persona.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace Personas
+{
+	public class Persona
+	{
+		public Persona(string vNombre, int vNdia, int vNmes)
+		{
+			this.Nombre = vNombre;
+			this.NDia = vNdia;
+			this.NMes = vNmes;
+		}
+		private string nombre;
+		private int ndia;
+		private int nmes;
+		public string Nombre
+        {
+			get
+            {
+				return nombre;
+            }
+			set 
+			{
+				nombre = value;
+			}
+        }
+		public int NDia
+		{
+			get
+			{
+				return ndia;
+			}
+			set
+			{
+				ndia = value;
+			}
+		}
+		public int NMes
+		{
+			get
+			{
+				return nmes;
+			}
+			set
+			{
+				nmes = value;
+			}
+		}
+	}
+}
+
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Personas;
+namespace eval03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su nombre ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese el dia de nacimiento en numero ");
+            int dia = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el mes de nacimiento en numero ");
+            int mes = Convert.ToInt32(Console.ReadLine());
+            Persona obPersona = new Persona (nombre, dia, mes);
+            string cadena1 = obPersona.Nombre + ", nacido(a) el " + obPersona.NDia + "/" + obPersona.NMes;
+            Console.WriteLine("Imprimiendo información... ");
+            Console.Write(cadena1);
+        }
+    }
+}
